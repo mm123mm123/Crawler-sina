4f7a830b82786e27be1b4c1c6bfab46bb62a1348
Felix <fiymi666@gmail.com>
2020-02-11 10:48:50 +0800

    UncheckedIOException
    Exception in thread "main" org.jsoup.UncheckedIOException
    java.io.IOException: Mark invalid

--- src/main/java/com/github/mm123mm123/Crawler/Crawler.java
+++ src/main/java/com/github/mm123mm123/Crawler/Crawler.java
@@ -19,44 +19,46 @@ import java.util.Set;
 public class Crawler {
     public void crawler() throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-
         List<String> linkPool = new ArrayList<>();
         Set<String> processedLinkPool = new HashSet<>();
         linkPool.add("https://sina.cn/");
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
+        while (!linkPool.isEmpty()) {
             String link = linkPool.remove(linkPool.size() - 1);
             if (processedLinkPool.contains(link)) {
                 continue;
             }
-            if (link.contains("https://news.sina.cn/") || link.equals("https://sina.cn/")) {
+            if (filterLinksConditions(link)) {
                 System.out.println(link);
                 processedLinkPool.add(link);
                 HttpGet httpGet = new HttpGet(link);
                 httpGet.addHeader("User_Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36");
 
                 try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-//                    System.out.println(response1.getStatusLine());
                     HttpEntity entity1 = response1.getEntity();
                     String html = EntityUtils.toString(entity1);
-                    Document doc = Jsoup.parse(html);
-                    String title = doc.body().select("h1").text();
-                    System.out.println(title);
-                    ArrayList<Element> tags = doc.select("a");
-                    for (Element taga : tags) {
-                        String newlink = taga.attr("href");
-                        linkPool.add(newlink);
-                    }
+                    prseHtml(html, linkPool);
                     EntityUtils.consume(entity1);
                 }
-
             } else {
                 continue;
             }
 
         }
 
+    }
+
+    private void prseHtml(String html, List<String> linkPool) {
+        Document doc = Jsoup.parse(html);
+        String title = doc.body().select("h1").text();
+        System.out.println(title);
+        ArrayList<Element> tags = doc.select("a");
+        for (Element taga : tags) {
+            String newlink = taga.attr("href");
+            linkPool.add(newlink);
+        }
+    }
+
+    private boolean filterLinksConditions(String link) {
+        return link.contains("https://news.sina.cn/") || link.equals("https://sina.cn/");
     }
 }
